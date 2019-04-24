b981ebb97d6428e19f793958cbf430f3d1223e40
Valeria Bocanegra <valeriliux@icloud.com>
2019-04-24 11:40:37 +0100

    Project-02 - Search for a Dog added

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -1,18 +1,46 @@
 import React from 'react'
+import axios from 'axios'
 
-const DogCard = ({ name, image }) => {
-  return (
-    <div className="card">
-      <div className="card-header">
-        <h3 className="card-header-title">{name}</h3>
+class DogCard extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      dog: null
+    }
+  }
+
+  componentDidMount(){
+    const dog = {}
+    axios.get('https://api.thedogapi.com/v1/images/search', {
+      params: {
+        breed_id: this.props.id
+      }
+    })
+      .then(res => {
+        dog.image = res.data[0].url
+        this.setState({ dog })
+      })
+
+  }
+
+  render() {
+    if(!this.state.dog) return null
+    console.log(this.state)
+    return(
+      <div className="card">
+        <div className="card-header">
+          <h3 className="columns is-multiline title is-2">{this.props.name}</h3>
+        </div>
+        <div className="card-image">
+          <figure className="columns is-multiline image is-1by1">
+            <img src={this.state.dog.image} />
+          </figure>
+        </div>
       </div>
-      <div id="card_images" className="card-image">
-        <figure className="image">
-          <img src={image} alt={name} />
-        </figure>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default DogCard
--- src/components/SearchDog.js
+++ src/components/SearchDog.js
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom'
 import React from 'react'
 import DogCard from './DogCard'
+import axios from 'axios'
 
 
 class SearchDog extends React.Component {
@@ -26,24 +27,59 @@ class SearchDog extends React.Component {
   }
 
   handleChange(e) {
-    this.props.history.push(`/dogs/${e.target.value}`)
+    const breedId = e.target.value
+    axios.get(`https://api.thedogapi.com/v1/breeds/${breedId}`)
+      .then(res => {
+        const dog = res.data
+        axios.get('https://api.thedogapi.com/v1/images/search', {
+          params: {
+            breed_id: breedId
+          }
+        })
+          .then(res => {
+            dog.image = res.data[0].url
+            this.setState({ dog })
+          })
+      })
   }
 
 
 
   render() {
     return(
-      <div className="select">
-        <select onChange={this.handleChange}>
-          {this.state.dogs.map(dog =>
-            <option key={dog.id} value={dog.id}>{dog.name}
-            </option>
-          )}
-        </select>
+      <div>
+        <div className="select">
+          <select onChange={this.handleChange}>
+            {this.state.dogs.map(dog =>
+              <option key={dog.id} value={dog.id}>{dog.name}
+              </option>
+            )}
+          </select>
+        </div>
+        {this.state.dog &&
+          <section className="section">
+            <div className="container">
+              <div className="level">
+                <div className="level-left">
+                  <h1 className="title is-1">{this.state.dog.name}</h1>
+                </div>
+              </div>
+              <div className="columns">
+                <div className="column is-half-desktop is-full-tablet">
+                  <figure className="image">
+                    <img src={this.state.dog.image} />
+                  </figure>
+                </div>
+                <div className="column is-half-desktop is-full-tablet">
+                  <h2 id="contenido" className="title is-3">Breed For:
+                    {this.state.dog.bred_for}</h2>
+                  <h2 id="contenido" className="title is-3">Temperament: {this.state.dog.temperament}</h2>
+                </div>
+              </div>
+            </div>
+          </section>
+        }
       </div>
-
-
-
     )
   }
 }
